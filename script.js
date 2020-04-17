707f5360188760e0936586722504682b2bac29db
Felipe Suknaic <fsuknaic@hotmail.com>
2020-04-17 14:31:37 -0500

    create function filterusers

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 // escreva seu codigo aqui
+
+const filterUsers = async (name) =>
+  fetch(`https://jsonplaceholder.typicode.com/users?name_like=${name}`, {
+    mode: "cors",
+  }).then((res) => res.json());
+
 let time = null;
 
 function handleKeyUp(event) {
   clearTimeout(time);
 
   time = setTimeout(() => {
-    console.log(event.target.value);
+    filterUsers(event.target.value).then((users) =>
+      console.log(users.map((user) => user.name))
+    );
   }, 1000);
 }
 
