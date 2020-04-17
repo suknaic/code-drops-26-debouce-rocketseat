ec3a6bf55edbd7a9f62650a8cb60fe94f0a2e0fd
Felipe Suknaic <fsuknaic@hotmail.com>
2020-04-17 13:09:52 -0500

    create function handlekeyup

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // escreva seu codigo aqui
+let time = null;
+
+function handleKeyUp(event) {
+  clearTimeout(time);
+
+  time = setTimeout(() => {
+    console.log(event.target.value);
+  }, 1000);
+}
+
+document.querySelector("input").addEventListener("keyup", handleKeyUp);
